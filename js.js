944b16ef15d14022fcd69099fdc82eac314d214f
noiivll <aufazundun31@gmail.com>
2024-09-12 13:40:01 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,19 @@
+//Toggle & Responsive Navigation
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector("nav");
+
+    burger.addEventListener("click",() => {
+        //Togle nav list and burger class
+        navLists.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+    navSlide();
+
+// clear form before unload
+window.onbeforeunload = () =>{
+    for (const form of document.getElementsByTagName("form")) {
+        form.reset();
+    }
+};
